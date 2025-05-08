fb86d5237ff39ee57b9efa49693058e361e9f095
alyhasansakr <alyhasansakr123@gmail.com>
2025-05-08 20:05:06 +0300

    added code documentation

--- src/Printable.java
+++ src/Printable.java
@@ -1,4 +1,4 @@
 public interface Printable {
-     String toStringShort();
-     String toStringLong();
+     String toStringShort();  // returns a string with just the id
+     String toStringLong();   // returns a string with all details, don't call toStringLong inside itself (infinite loop)
 }
